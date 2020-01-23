6a549eb200c901da0d97afe46477f50d794b8b17
Nando Theessen <nando.theessen@gmail.com>
2020-01-24 00:23:23 +0100

    build function readin json / yaml files and return bytestring containing either

--- pkg/reader/reader.go
+++ pkg/reader/reader.go
@@ -7,10 +7,20 @@ import (
 // ProcessFile takes a filename as argument and returns a bytearray with the 
 // contents of that file or an error
 func ProcessFile(filename string) (contents []byte, err error) {
-	contents, err = ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
+	if filename == "" {
+		yamlstring := `
+		- path: /urlshort
+		  url: https://github.com/gophercises/urlshort
+		- path: /urlshort-final
+		  url: https://github.com/gophercises/urlshort/tree/solution
+		`
+		contents = []byte(yamlstring)
+	} else {
+		contents, err = ioutil.ReadFile(filename)
+		if err != nil {
+			return nil, err
+		
+		}
 	}
-
-	return contents, nil
+		return contents, nil
 }
