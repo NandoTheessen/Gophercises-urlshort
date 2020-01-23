aebd9daa86e45f54d76b9a70285e9bda2bfa1b15
Nando Theessen <nando.theessen@gmail.com>
2020-01-24 00:30:20 +0100

    Add ProcessYaml function to reader package

--- pkg/reader/reader.go
+++ pkg/reader/reader.go
@@ -2,6 +2,7 @@ package reader
 
 import (
 	"io/ioutil"
+	"gopkg.in/yaml.v2"
 )
 
 // ProcessFile takes a filename as argument and returns a bytearray with the 
@@ -23,4 +24,25 @@ func ProcessFile(filename string) (contents []byte, err error) {
 		}
 	}
 		return contents, nil
+}
+
+type pathURL struct {
+	Path string `yaml:"path"`
+	URL string `yaml:"url"`
+}
+
+// ProcessYaml takes in an array of bytes containing yaml file content & parses it
+// into a pathsToUrls map
+func ProcessYaml(yml []byte) (pathsToUrls map[string]string, err error) {
+	var output []pathURL
+	pathsToUrls = make(map[string]string)
+	err = yaml.Unmarshal(yml, &output)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, val := range output {
+		pathsToUrls[val.Path] = val.URL
+	}
+	return pathsToUrls, nil
 }
