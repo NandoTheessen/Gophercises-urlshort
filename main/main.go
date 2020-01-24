14300227c3b6ad18758f9187bb5011f7de123a67
Nando Theessen <nando.theessen@gmail.com>
2020-01-24 01:15:25 +0100

    Move functionality to reader package, handling file-io & parsing of yaml + json

--- main/main.go
+++ main/main.go
@@ -5,15 +5,16 @@ import (
 	"flag"
 	"net/http"
 
-	"github.com/NandoTheessen/Gophercises---urlshort/urlshort"
+	"github.com/NandoTheessen/Gophercises-urlshort/pkg/urlshort"
+	"github.com/NandoTheessen/Gophercises-urlshort/pkg/reader"
 )
 
 var file string
 var format string
 
 func init() {
-	flag.StringVar(&file, "pathfile", "", "provide a json | yaml file composed of sequence of path & url mappings")
-	flag.StringVar(&format, "format", "", "file format of the provided pathfile")
+	flag.StringVar(&file, "file", "none", "provide a json | yaml file composed of sequence of path & url mappings")
+	flag.StringVar(&format, "format", "yaml", "file format of the provided pathfile")
 	flag.Parse()
 }
 
@@ -28,30 +29,24 @@ func main() {
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
-	var yaml []byte
-	var err error
+
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
-	if file != "" {
-		yaml, err = reader.ProcessFile(file)
-		
-	} else {
-		yamlstring := `
-		- path: /urlshort
-		  url: https://github.com/gophercises/urlshort
-		- path: /urlshort-final
-		  url: https://github.com/gophercises/urlshort/tree/solution
-		`
-		yaml = []byte(yamlstring)
+	byteString, err := reader.ProcessFile(file)
+	if err != nil {
+		fmt.Println(err)
 	}
 	
-	yamlHandler, err := urlshort.YAMLHandler(yaml, mapHandler)
-	if err != nil {
-		panic(err)
+	if format == "yaml" {
+		yamlHandler, err := urlshort.YAMLHandler(byteString, mapHandler)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Starting the server on :8080")
+		http.ListenAndServe(":8080", yamlHandler)
+	} else {
+		// build json handler
 	}
-
-	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
 }
 
 func defaultMux() *http.ServeMux {
